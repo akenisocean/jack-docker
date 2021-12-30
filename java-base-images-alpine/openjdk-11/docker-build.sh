55b5871470bb290ea3e4192a1da00189bdd1ffe4
akenisocean <jc164214878@163.com>
2021-12-30 17:41:46 +0800

    增加了es的最新7.16.2版本的security安全认证，并且增加了kibana管理界面的基础镜像和kibana

--- java-base-images-alpine/openjdk-11/docker-build.sh
+++ java-base-images-alpine/openjdk-11/docker-build.sh
@@ -1,3 +1,6 @@
 
 # 构建  docker tag SOURCE_IMAGE[:TAG] harbor.jkservice.org/di/REPOSITORY[:TAG]
 docker build --tag harbor.jkservice.org/di/openjdk:11-jdk-alpine .
+
+
+docker push harbor.jkservice.org/di/openjdk:11-jdk-alpine
