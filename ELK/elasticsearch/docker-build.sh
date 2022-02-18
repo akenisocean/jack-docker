69dca2607cc7d38796ceb4cbdc3cdd75075cd850
akenisocean <jc164214878@163.com>
2022-02-18 17:32:01 +0800

    增加了es的最新7.16.3版本的security安全认证，并且增加了kibana管理界面的基础镜像和kibana

--- ELK/elasticsearch/docker-build.sh
+++ ELK/elasticsearch/docker-build.sh
@@ -1,4 +1,4 @@
-VERSION=7.16.2
+VERSION=7.16.3
 docker build --tag harbor.jkservice.org/di/security-elasticsearch:$VERSION .
 # 推送私服仓库
 docker push harbor.jkservice.org/di/security-elasticsearch:$VERSION
